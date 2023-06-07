f3d8485ea944554f1b77cce8264e987e4f00489d
eosull <osullivaneoin442@gmail.com>
2023-06-07 18:42:55 +0000

    build: initial setup, installation and model build

---
+++ eosull_portfolio/portfolio/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils.text import slugify
+
+class Projects(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    date = models.DateField()
+    description = models.TextField()
+    code_link = models.URLField(max_length=200, blank=True)
+    live_link = models.URLField(max_length=200, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+
+    def __str__(self):
+        return self.title
+    
+    def live(self):
+        if live_link:
+            return True
+        else:
+            return False
