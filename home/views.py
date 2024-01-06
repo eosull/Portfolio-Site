e4fdc469574b21c6e78e4ef92c9e8f27f7607d76
eosull <osullivaneoin442@gmail.com>
2024-01-06 16:23:03 +0000

    style:add experience summaries

--- home/views.py
+++ home/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from portfolio.views import Project
+from portfolio.models import Project
+from cv.models import ProfessionalExperience, AcademicExperience
 
 def home(request):
     # View returning index page
     projects = Project.objects.order_by('-date')[:3]
+    prof_exp = ProfessionalExperience.objects.all()
+    acad_exp = AcademicExperience.objects.all()
 
     context = {
         'projects': projects,
+        'prof_exp': prof_exp,
+        'acad_exp': acad_exp,
     }
 
     return render(request, 'home/index.html', context)
