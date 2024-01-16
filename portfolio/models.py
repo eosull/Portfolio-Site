38f26d49ef68e38aa7318cc938a12c45fae1ef2e
eosull <osullivaneoin442@gmail.com>
2024-01-16 16:56:13 +0000

    style:add projects detail styling and ody text

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,9 @@ class Project(models.Model):
     slug = models.SlugField(max_length=200, unique=True, blank=True)
     date = models.DateField()
     description = models.TextField()
+    body_text_1 = models.TextField(blank=True, null=True)
+    body_text_2 = models.TextField(blank=True, null=True)
+    body_text_3 = models.TextField(blank=True, null=True)
     code_link = models.URLField(max_length=200, blank=True)
     live_link = models.URLField(max_length=200, blank=True)
 
