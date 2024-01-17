b236238f3206deca7e4728736412168af9c7a260
eosull <osullivaneoin442@gmail.com>
2024-01-17 16:46:29 +0000

    style:adjust experiences styling

--- home/views.py
+++ home/views.py
@@ -5,8 +5,8 @@ from cv.models import ProfessionalExperience, AcademicExperience, Personal
 def home(request):
     # View returning index page
     projects = Project.objects.order_by('-date')[:3]
-    prof_exp = ProfessionalExperience.objects.all()
-    acad_exp = AcademicExperience.objects.all()
+    prof_exp = ProfessionalExperience.objects.order_by('-end_date')
+    acad_exp = AcademicExperience.objects.order_by('-end_date')
     personal = Personal.objects.get(name="Eoin O'Sullivan")
 
     context = {
