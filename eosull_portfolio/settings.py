4166013288de18c8bbd58c3a24723244b4db3152
eosull <osullivaneoin442@gmail.com>
2024-02-07 10:07:11 +0000

    style:update letter spacing, nav hover

--- eosull_portfolio/settings.py
+++ eosull_portfolio/settings.py
@@ -100,7 +100,6 @@ WSGI_APPLICATION = 'eosull_portfolio.wsgi.application'
 DATABASES = {
     'default':
         dj_database_url.parse(os.environ.get("DATABASE_URL"))
-        # dj_database_url.parse('postgres://oioizjre:R9w_cUTxN0F_T5wjEwSLll6urSEc9N9B@lucky.db.elephantsql.com/oioizjre')
 }
 
 
@@ -143,11 +142,13 @@ DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
+# For local CSS work
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 # AWS Configuration
-# AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_ACCESS_KEY_ID = "AKIA3MT65A44BTPKH5QQ"
-AWS_SECRET_ACCESS_KEY = "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"
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'eosull-portfolio'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_SIGNATURE_NAME = 's3v4',
