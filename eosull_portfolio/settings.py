81d536b62d85686db27ff5590148e15e6febfbb2
eosull <osullivaneoin442@gmail.com>
2024-02-10 17:31:35 +0000

    build:update debug var

--- eosull_portfolio/settings.py
+++ eosull_portfolio/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-eosull-portfoliosite-id5edz7n5bf.ws-eu107.gitpod.io',
                  '8000-eosull-portfoliosite-id5edz7n5bf.ws-eu108.gitpod.io',
